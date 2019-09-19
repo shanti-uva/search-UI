cba5c4b81ef67035d11bad4fdbc34aff6645a284
ys2n <ys2n@virginia.edu>
2019-09-19 16:52:25 -0400

    modified facets.  started advanced facets

--- kmapsSolrUtil.js
+++ kmapsSolrUtil.js
@@ -31,15 +31,15 @@ class KmapsSolrUtil {
             "pageSize": 100,																	// Results per page
             "query": { 																		// Current query
                 "text": "",																			// Search word
-                "place": [],																			// Places
-                "collection": [],																		// Collections
-                "language": [],																		// Languages
-                "feature": [],																			// Feature types
-                "subject": [],																			// Subjects
-                "term": [],																			// Terms
-                "relationship": [],																	// Relationships
-                "user": [],																			// Users
-                "asset": [],																			// Assets
+                "places": [],																			// Places
+                "collections": [],																		// Collections
+                "languages": [],																		// Languages
+                "features": [],																			// Feature types
+                "subjects": [],																			// Subjects
+                "terms": [],																			// Terms
+                "relationships": [],																	// Relationships
+                "users": [],																			// Users
+                "assets": [],																			// Assets
                 "dateStart": "",
                 "dateEnd": ""															// Beginning and ending dates
             }
@@ -57,18 +57,39 @@ class KmapsSolrUtil {
                 "limit": 300,
                 "type": "terms",
                 "field": "kmapid",
-                "prefix": "places"
+                "prefix": "places",
+                "facet": {
+                    "title": {
+                        "limit":1,
+                        "type":"terms",
+                        "field":"title"
+                    }
+                }
             },
             "subjects": {
                 "limit": 300,
                 "type": "terms",
                 "field": "kmapid",
-                "prefix": "subjects"
+                "prefix": "subjects",
+                "facet": {
+                    "title": {
+                        "limit":1,
+                        "type":"terms",
+                        "field":"title"
+                    }
+                }
             },
-            "collection_title": {
+            "collection_nid": {
                 "limit": 300,
                 "type": "terms",
-                "field": "collection_title"
+                "field": "collection_nid",
+                "facet": {
+                    "collection_title": {
+                        "limit":1,
+                        "type":"terms",
+                        "field":"collection_title"
+                    }
+                }
             },
             "asset_subtype": {
                 "limit": 300,
@@ -99,8 +120,29 @@ class KmapsSolrUtil {
             }
         };
 
-        console.log("construtced");
 
+        this.facetAdvancedJSON = {
+            related:{
+                domain:{blockChildren:"block_type:parent"},
+                type:"terms",
+                field:"related_subjects_relation_code_s",
+                limit:-1,
+                facet:{
+                    related_subject: {
+                        type: "terms",
+                        field: "related_subjects_id_s",
+                        limit: -1,
+                        facet: {
+                            related_subject_name: {
+                                type: "terms",
+                                field: "related_subjects_header_s",
+                                limit: 1
+                            }
+                        }
+                    }
+                }
+            }
+        };
     }
 
     createBasicQuery(state) {
@@ -170,4 +212,83 @@ class KmapsSolrUtil {
         var url = new URL(baseurl + "?" + params.toString());
         return url;
     }
+
+
+    createAdvancedFacetQuery(state) {
+        state = $.extend(true, {}, this.defaultState, state);
+
+        state.solrUrl= "https://ss251856-us-east-1-aws.measuredsearch.com/solr/kmterms_dev/select"
+
+        // create request object
+
+        var searchstring = state.query.text || "";
+        var page = state.page || 0;
+        var pageSize = state.pageSize || 100;
+        console.log (JSON.stringify(state));
+        var starts = searchstring + "*";
+        var search = "*" + searchstring + "*";
+        var slashy = searchstring + "/";
+        if ($.type(searchstring) === "undefined" || searchstring.length === 0 ) {
+            searchstring = search = slashy = "*";
+        }
+
+        var req =
+            {
+                // search: tweak for scoping later
+                "q": "(" +
+                    " header:${xact}^100" +
+                    " header:${slashy}^100" +
+                    // " names_txt:${xact}^90" +
+                    " header:${starts}^80" +
+                    // " names_txt:${starts}^70" +
+                    " header:${search}^10" +
+                    // " caption:${search}" +
+                    // " summary:${search}" +
+                    // " names_txt:${search}" +
+                    ")",
+
+                // search strings
+                "xact": searchstring,
+                "starts": starts,
+                "search": search,
+                "slashy": slashy,
+
+                // generic settings (maybe tweak for efficiency later)
+                "fl": "*,[child parentFilter=block_type:parent]",
+                "wt": "json",
+
+                // paging
+                "start": page,
+                "rows": pageSize,
+
+                // facets
+                "facet": "on",
+                "json.facet": JSON.stringify(this.facetAdvancedJSON),
+
+                // highlighting
+                // "hl": "on",
+                // "hl.method": "unified",
+                // "hl.fl": "header,caption,summary,names_txt",
+                // "hl.fragsize": 0,
+                // "hl.tag.pre": "<mark>",
+                // "hl.tag.post": "</mark>",
+
+                // debug settings  -- set both to false in production?
+                "echoParams": "explicit",
+                "indent": "true"
+            };
+
+        var baseurl = state.solrUrl;
+        var params = new URLSearchParams(req);
+        var url = new URL(baseurl + "?" + params.toString());
+        return url;
+    }
+
+
+
+
+
+
+
+
 }
