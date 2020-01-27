e8dfb9e89391e64e1547885a20cb50673f5614f1
ys2n <ys2n@virginia.edu>
2020-01-27 16:08:19 -0500

    fixed sorting problem

--- kmapsSolrUtil.js
+++ kmapsSolrUtil.js
@@ -595,9 +595,9 @@ class KmapsSolrUtil {
 
 			"&facet.mincount=2" +
 			"&facet.limit=-1" +
-		    "&sort=" + levelField + "+ASC" +
+		    // "&sort=" + levelField + "+ASC" +
 			"&sort=position_i+asc" +
-		    "&sort=header+asc" +
+		    // "&sort=header+asc" +
 			"&facet.sort=" + ancestorField +"+ASC" +
 			"&facet.field={!ex=hoot}" + ancestorField +
             "&json.facet=" + JSON.stringify(jsonfacetblob) +
