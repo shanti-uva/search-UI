8905191ba5670cc8ce16289c7ab10f4fc4aa39f9
bferster <bferster@stagetools.com>
2020-02-05 11:58:41 -0500

    collections

--- collections.js
+++ collections.js
@@ -18,12 +18,10 @@ class Collections  {
 	{
 		this.div=sui.pages.div;																	// Div to hold page (same as Pages class)
 		this.content=["",""," "];																// Content pages
-		this.noSubs=1;
 	}
 
 	Draw(o)																					// DRAW SOURCE PAGE FROM KMAP
 	{
-		this.noSubs=1;
 		let a=sui.assets[o.asset_subtype.toLowerCase()];
 		var str=`<div class='sui-sources' id='sui-collections'>
 		<span style='font-size:24px;color:${a.c};vertical-align:-4px'>${a.g}</span>
@@ -31,12 +29,10 @@ class Collections  {
 		&nbsp;&nbsp;(${o.asset_subtype.toUpperCase()})
 		<hr style='border-top: 1px solid ${sui.assets[o.asset_type].c}'>
 		<div class='sui-sourceSec' id='sui-srcSec'></div><br>`;
-		if (o.subcollection_name_ss && o.subcollection_name_ss.length) 	this.noSubs=0;			// If subcollections
 		if (o.url_thumb && !o.url_thumb.match(/gradient.jpg/)) str+="<img src='"+o.url_thumb+"' style='width:33%;padding-right:12px;vertical-align:top'>";
 		if (o.summary)
 			str+=`<div class='sui-sourceText' style='display:inline-block;width:calc(66% - 12px)'>${o.summary}</div><br><br>`;
-		if (this.noSubs)	str+=sui.pages.DrawTabMenu(["MEMBERS","DETAILS"])+"</div>";			// Add  tab menu
-		else				str+=sui.pages.DrawTabMenu(["SUBCOLLECTIONS","MEMBERS","DETAILS"])+"</div>";// Add tab menu w/ subs
+		str+=sui.pages.DrawTabMenu(["RELATED COLLECTIONS","MEMBERS","DETAILS"])+"</div>";		// Add tab menu
 		$(this.div).html(str.replace(/\t|\n|\r/g,""));											// Remove format and add to div	
 
 		$("[id^=sui-tabTab]").on("click", (e)=> {												// ON TAB CLICK
@@ -51,33 +47,42 @@ class Collections  {
 			
 		sui.pages.DrawRelatedAssets(o);															// Draw related assets menu if active
 		this.AddTabContent(o);																	// Add contents of tabs
-		this.ShowTab(2-this.noSubs);															// Show details
+		this.ShowTab(0);																		// Show collections
 		}
 	
 	AddTabContent(o)																		// GET TAB CONTENTS
 	{
-		let i,a;
-		let n=1-this.noSubs;																	// Point at members tav
+		let i,a,tab=0;
+		this.content[0]="<br>";
 		let base=o.asset_subtype.toLowerCase()+"-collection-";									// Collections base id
-		if (!this.noSubs) {																		// If subcollections
+			try {if (o.collection_title_path_ss)	{												// If a parent
+			a=o.collection_uid_path_ss;															// Get parent id
+			this.content[0]+=`<div><a href='#p=${a}' id='sui-spItem-${a}+"'>${o.collection_title_path_ss}</a></div>`; // Add
+			tab++;																				// Next tab	
+		} } catch(e){}
+	
+		this.content[0]+=`<div style='margin-left:${tab*24}px'><b>${o.title[0]}</b></div>`; 			// Add
+	
+		if (o.subcollection_name_ss && o.subcollection_name_ss.length) 	{						// If subcollections
+			tab++;
 			for (i=0;i<o.subcollection_name_ss.length;++i) {									// For each sub												
 				a=base+o.subcollection_id_is[i];												// Make full id
-				this.content[0]+=`<br><a href='#p=${a}' id='sui-spItem-${a}+"'>${o.subcollection_name_ss[i]}</a>`; // Add
+				this.content[0]+=`<div style='margin-left:${tab*24}px'><a  href='#p=${a}' id='sui-spItem-${a}+"'>${o.subcollection_name_ss[i]}</a></div>`; // Add
 				}
-			this.content[0]+="<br><br>";
 			}	
+		this.content[0]+="<br><br>";
 		
 		if (o.members_name_ss && o.members_name_ss.length) {									// If members
 			for (i=0;i<o.members_name_ss.length;++i)											// For each member												
-				this.content[n]+="<br>"+o.members_name_ss[i]+" &nbsp; ("+o.members_id_ss[i]+")"; // Add
-			this.content[n]+="<br><br>";
+				this.content[1]+="<br>"+o.members_name_ss[i]+" &nbsp; ("+o.members_id_ss[i]+")"; // Add
+			this.content[1]+="<br><br>";
 			}	
-		this.content[++n]="<br>";																// Advance to details tab
-		if (o.node_user_full_s) this.content[n]+=sui.pages.DrawItem("&#xe600","OWNER",o.node_user_full_s+" &nbsp; ("+o.node_user+")","","sui-pageLab",1);	// Owner
-		if (o.collection_visibility_s) this.content[n]+=sui.pages.DrawItem("&#xe622","VISIBILITY",o.collection_visibility_s.substr(0,1).toUpperCase()+o.collection_visibility_s.substr(1),"","sui-pageLab",1);	// Visibility
-		if (o.asset_subtype) this.content[n]+=sui.pages.DrawItem(sui.assets[o.asset_subtype.toLowerCase()].g,"TYPE",o.asset_subtype,"","sui-pageLab",1);	// Type
-		if (o.collection_title) this.content[n]+=sui.pages.DrawItem(sui.assets[o.asset_subtype.toLowerCase()].g,"COLLECTION",`<a title='Collection' id='sui-imgCol'	href='#p=${o.collection_uid_s}'>${o.collection_title}</a>`,"","sui-pageLab",1);	// Type
-		this.content[n]+="<br>";
+		this.content[2]="<br>";																// Advance to details tab
+		if (o.node_user_full_s) this.content[2]+=sui.pages.DrawItem("&#xe600","OWNER",o.node_user_full_s+" &nbsp; ("+o.node_user+")","","sui-pageLab",1);	// Owner
+		if (o.collection_visibility_s) this.content[2]+=sui.pages.DrawItem("&#xe622","VISIBILITY",o.collection_visibility_s.substr(0,1).toUpperCase()+o.collection_visibility_s.substr(1),"","sui-pageLab",1);	// Visibility
+		if (o.asset_subtype) this.content[2]+=sui.pages.DrawItem(sui.assets[o.asset_subtype.toLowerCase()].g,"TYPE",o.asset_subtype,"","sui-pageLab",1);	// Type
+		if (o.collection_title) this.content[2]+=sui.pages.DrawItem(sui.assets[o.asset_subtype.toLowerCase()].g,"COLLECTION",`<a title='Collection' id='sui-imgCol'	href='#p=${o.collection_uid_s}'>${o.collection_title}</a>`,"","sui-pageLab",1);	// Type
+		this.content[2]+="<br>";
 	}
 
 	ShowTab(which) 																			// SHOW TAB
